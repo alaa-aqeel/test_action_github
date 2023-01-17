ce230fe752dddbd35edb8da10dea11ea2f545f35
Alaa Aqeel <alaa.21.iraq@gmail.com>
2023-01-17 21:45:47 +0300

    modified:   topdo/tests.py

--- topdo/tests.py
+++ topdo/tests.py
@@ -5,4 +5,4 @@ class AppTestCase(TestCase):
 
     def test_animals_can_speak(self):
         """Animals that can speak are correctly identified"""
-        self.assertEqual(1, 0)
+        self.assertEqual(1, 1)
